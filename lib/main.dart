9f93747a5be24f52387964d5efb5b8a05fc76779
Eduardo Alonso <edusantos1998@gmail.com>
2022-06-28 08:34:11 -0300

    bug

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flu_mobx/imc/imc_page.dart';
 import 'package:flu_mobx/modelo_observable/modelo_observable_page.dart';
+import 'package:flu_mobx/observable_future/future_page.dart';
 import 'package:flu_mobx/observables/observable_list_page.dart';
 import 'package:flutter/material.dart';
 
@@ -9,6 +10,6 @@ void main() {
     theme: ThemeData(
       primarySwatch: Colors.blue,
     ),
-    home: ModeloObservablePage(),
+    home: FuturePage(),
   ));
 }
---
+++ lib/observable_future/future_page.dart
@@ -0,0 +1,62 @@
+import 'package:flu_mobx/observable_future/future_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+
+class FuturePage extends StatefulWidget {
+  const FuturePage({Key? key}) : super(key: key);
+
+  @override
+  State<FuturePage> createState() => _FuturePageState();
+}
+
+class _FuturePageState extends State<FuturePage> {
+  final _controller = FutureController();
+
+  @override
+  void initState() {
+    //_controller.buscarNome();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Future Page'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _controller.buscarNome();
+        },
+        child: const Icon(Icons.refresh),
+      ),
+      body: Observer(
+        builder: (_) {
+          return FutureBuilder<String>(
+            future: _controller.buscarNome,
+            builder: (context, snapshot) {
+              switch (snapshot.connectionState) {
+                case ConnectionState.none:
+                  return const SizedBox.shrink();
+                case ConnectionState.waiting:
+                case ConnectionState.active:
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                case ConnectionState.done:
+                  if (snapshot.hasData) {
+                    return Center(
+                      child: Text(snapshot.data!),
+                    );
+                  }
+                  return const Center(
+                    child: Text('Nome não encontrado'),
+                  );
+              }
+            },
+          );
+        },
+      ),
+    );
+  }
+}
